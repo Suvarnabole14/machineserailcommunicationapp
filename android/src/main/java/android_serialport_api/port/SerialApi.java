59e82e3fcd5ec8c45ada00bda9639c7323671ed1
Suvarna bole <suvarna.b@cheriseglobal.com>
2024-07-16 09:48:40 +0530

    115200 baud rate using for serial communications

--- android/src/main/java/android_serialport_api/port/SerialApi.java
+++ android/src/main/java/android_serialport_api/port/SerialApi.java
@@ -62,7 +62,7 @@ class SerialApi {
             return open;
         }
         try {
-            serialPort = new android_serialport_api.SerialPort(new File(port), 9600, 0);
+            serialPort = new android_serialport_api.SerialPort(new File(port), 115200, 0);
             if (serialPort == null) {
 
                 log(SerialApiManager.port, port, isAscii, new StringBuffer().append("Boot failure：SerialPort == null"));
